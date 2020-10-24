57f9afc12d6a88f3b79c852266901748afd314f0
Kimkwanghyun <66558399+rhkdgus7676@users.noreply.github.com>
2020-10-25 00:17:04 +0900

    input dialog added

---
+++ programming/src/okJavaInput.java
@@ -0,0 +1,33 @@
+import javax.swing.JOptionPane;
+
+import org.opentutorials.iot.DimmingLights;
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Lighting;
+import org.opentutorials.iot.Security;
+
+public class okJavaInput {
+
+	public static void main(String[] args) {
+		
+		String id = JOptionPane.showInputDialog("enter a id");
+		String bright = JOptionPane.showInputDialog("set brightness");
+		
+		//elevator
+		Elevator myElevator = new Elevator(id);
+		myElevator.callForUp(1);
+		
+		//security
+		
+		Security mySecurity = new Security(id);
+		mySecurity.off();
+		
+		//light
+		Lighting hallLamp = new Lighting(id+" / Hall Lamp");
+		hallLamp.on();
+		
+		DimmingLights moodLamp = new DimmingLights(id+" moodLamp");
+		moodLamp.setBright(Double.parseDouble(bright));
+		moodLamp.on();
+	}
+
+}
