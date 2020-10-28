6a78438a15b108c3c5684010e9c3bd55813e4f24
Kimkwanghyun <66558399+rhkdgus7676@users.noreply.github.com>
2020-10-28 23:20:04 +0900

    making definitions to variables

--- MyApp/src/AccountingApp.java
+++ MyApp/src/AccountingApp.java
@@ -2,11 +2,25 @@
 public class AccountingApp {
 
 	public static void main(String[] args) {
-		System.out.println("Value of supply : "+ 10000.0 );
-		System.out.println("VAT : "+(10000.0*0.1) );
-		System.out.println("TOTAL : "+(10000.0+10000.0*1) );
-		System.out.println("Expense : "+ (10000.0*0.3) );
-		System.out.println("Income : "+ (10000.0 - 10000.0*0.3) );
+		double ValueOfSupply = 10000.0;		
+		double vatRate = 0.1;
+		double expenseRate = 0.3;
+		double VAT = ValueOfSupply*vatRate;
+		double total = ValueOfSupply+ValueOfSupply*0.1;
+		double expense = ValueOfSupply*expenseRate;
+		double income = ValueOfSupply - ValueOfSupply*expenseRate;
+		double dividend1 = income * 0.5;
+		double dividend2 = income * 0.3;
+		double dividend3 = income * 0.2;
+		
+		System.out.println("Value of supply : "+ ValueOfSupply );				
+		System.out.println("VAT : "+VAT );		
+		System.out.println("TOTAL : "+total );		
+		System.out.println("Expense : "+ expense );
+		System.out.println("Income : "+ income );		
+		System.out.println("Dividend1 : "+ dividend1 );	
+		System.out.println("Dividend2 : "+ dividend2 );		
+		System.out.println("Dividend3 : "+ dividend3 );
 	}
 
 }
